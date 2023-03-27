743bfa09345a3264f2e201ce03b26530ba259742
Tenzin Dawa <tnzngdw@gmail.com>
2023-03-27 14:05:37 +0530

    Delete cowin-report-service directory

--- cowin-report-service/src/main/java/com/example/cowinreportservice/constants/ReportCsvConstants.java
+++
@@ -1,6 +0,0 @@
-package com.example.cowinreportservice.constants;
-
-public class ReportCsvConstants {
-    public static final String UPLOAD_PATH = "/upload";
-    public static final String API_VERSION = "/api/v1";
-}
--- cowin-report-service/src/main/java/com/example/cowinreportservice/controller/ReportJsonDownloadController.java
+++
@@ -1,8 +0,0 @@
-package com.example.cowinreportservice.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class ReportJsonDownloadController {
-
-}
