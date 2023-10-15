307cfe304d5a9a41046f1c9f5477e282a4771231
Tenzin Dawa <tnzngdw@gmail.com>
2023-10-15 13:36:04 -0400

    Update TransformUtils.java

--- cowin-report-service/src/main/java/com/example/cowinreportservice/testingunit/utils/TransformUtils.java
+++ cowin-report-service/src/main/java/com/example/cowinreportservice/testingunit/utils/TransformUtils.java
@@ -8,6 +8,19 @@ import java.io.IOException;
 
 @Slf4j
 public class TransformUtils {
+
+    private TransformUtils() {
+        // Private constructor to prevent instantiation, as it's a utility class
+        throw new IllegalStateException("Utility class");
+    }
+
+    /**
+     * Convert MultipartFile to String.
+     *
+     * @param file MultipartFile to be converted.
+     * @return String representation of the MultipartFile.
+     * @throws ReportJsonException if an error occurs during conversion.
+     */
     public static String toString(MultipartFile file) {
         try {
             if (!file.isEmpty()) {
@@ -15,9 +28,19 @@ public class TransformUtils {
                 return new String(bytes);
             }
         } catch (IOException exception) {
-            log.error(exception.getLocalizedMessage());
-            throw new ReportJsonException("Error occurred while converting to JSON String : {}", exception.getLocalizedMessage());
+            handleException(exception);
         }
         return "";
+    }
+
+    /**
+     * Handles exceptions by logging and throwing a custom exception.
+     *
+     * @param exception The exception to handle.
+     * @throws ReportJsonException with the appropriate message.
+     */
+    private static void handleException(IOException exception) {
+        log.error(exception.getLocalizedMessage());
+        throw new ReportJsonException("Error occurred while converting to JSON String: " + exception.getLocalizedMessage());
     }
 }
