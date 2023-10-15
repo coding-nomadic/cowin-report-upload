0246dc6f08bb9fe94a779fcc0cc05429d83022cf
Tenzin Dawa <tnzngdw@gmail.com>
2023-10-15 13:36:48 -0400

    Update JsonRetrieval.java

--- cowin-report-service/src/main/java/com/example/cowinreportservice/testingunit/utils/JsonRetrieval.java
+++ cowin-report-service/src/main/java/com/example/cowinreportservice/testingunit/utils/JsonRetrieval.java
@@ -7,42 +7,46 @@ import com.example.cowinreportservice.testingunit.models.JsonRoot;
 public class JsonRetrieval {
 
     private JsonRetrieval() {
+        // Private constructor to prevent instantiation
+        throw new IllegalStateException("Utility class");
     }
 
     /**
-     * 
-     * @param type
-     * @param jsonRoot
-     * @return
+     * Retrieves data from JsonRoot based on the specified type.
+     *
+     * @param type The type of data to retrieve.
+     * @param jsonRoot The JsonRoot object containing the data.
+     * @return The retrieved data based on the specified type.
+     * @throws ReportJsonException if an invalid type is provided.
      */
     public static Object getObject(String type, JsonRoot jsonRoot) {
         switch (type) {
-        case ReportJsonConstants.VACCINE_DONE_TIME_TODAY:
-            return jsonRoot.vaccinationDoneByTimeToday;
-        case ReportJsonConstants.LAST_30_DAYS_VACCINATION:
-            return jsonRoot.last30DaysVaccination;
-        case ReportJsonConstants.WEEKLY_VACCINATION:
-            return jsonRoot.weeklyAgeWiseVaccination;
-        case ReportJsonConstants.VACCINATION_BY_TIME_AGE_WISE_TODAY:
-            return jsonRoot.vaccinationDoneByTimeAgeWiseToday;
-        case ReportJsonConstants.LAST_30_DAYS_AGE_WISE_VACCINATION:
-            return jsonRoot.last30DaysAgeWiseVaccination;
-        case ReportJsonConstants.WEEKLY_AGE_WISE_VACCINATION:
-            return jsonRoot.weeklyAgeWiseVaccination;
-        case ReportJsonConstants.REGISTRATION_DONE_BY_TIME_TODAY:
-            return jsonRoot.registrationDoneByTimeToday;
-        case ReportJsonConstants.LAST_30_DAYS_REGISTRATION:
-            return jsonRoot.last30DaysRegistration;
-        case ReportJsonConstants.WEEKLY_REGISTRATION:
-            return jsonRoot.weeklyRegistration;
-        case ReportJsonConstants.LAST_30_DAYS_AEFI_REPORTED:
-            return jsonRoot.last30DaysAefiReported;
-        case ReportJsonConstants.VACCINATION_BY_AGE:
-            return jsonRoot.vaccinationByAge;
-        case ReportJsonConstants.STATE_WISE_VACCINATION:
-            return jsonRoot.stateWiseVaccination;
-        default:
-            throw new ReportJsonException("Invalid Type !", "102");
+            case ReportJsonConstants.VACCINE_DONE_TIME_TODAY:
+                return jsonRoot.getVaccinationDoneByTimeToday();
+            case ReportJsonConstants.LAST_30_DAYS_VACCINATION:
+                return jsonRoot.getLast30DaysVaccination();
+            case ReportJsonConstants.WEEKLY_VACCINATION:
+                return jsonRoot.getWeeklyAgeWiseVaccination();
+            case ReportJsonConstants.VACCINATION_BY_TIME_AGE_WISE_TODAY:
+                return jsonRoot.getVaccinationDoneByTimeAgeWiseToday();
+            case ReportJsonConstants.LAST_30_DAYS_AGE_WISE_VACCINATION:
+                return jsonRoot.getLast30DaysAgeWiseVaccination();
+            case ReportJsonConstants.WEEKLY_AGE_WISE_VACCINATION:
+                return jsonRoot.getWeeklyAgeWiseVaccination();
+            case ReportJsonConstants.REGISTRATION_DONE_BY_TIME_TODAY:
+                return jsonRoot.getRegistrationDoneByTimeToday();
+            case ReportJsonConstants.LAST_30_DAYS_REGISTRATION:
+                return jsonRoot.getLast30DaysRegistration();
+            case ReportJsonConstants.WEEKLY_REGISTRATION:
+                return jsonRoot.getWeeklyRegistration();
+            case ReportJsonConstants.LAST_30_DAYS_AEFI_REPORTED:
+                return jsonRoot.getLast30DaysAefiReported();
+            case ReportJsonConstants.VACCINATION_BY_AGE:
+                return jsonRoot.getVaccinationByAge();
+            case ReportJsonConstants.STATE_WISE_VACCINATION:
+                return jsonRoot.getStateWiseVaccination();
+            default:
+                throw new ReportJsonException("Invalid Type!", "102");
         }
     }
 }
