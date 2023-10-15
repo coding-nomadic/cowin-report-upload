57695ca4559e040c7156ff87f4ac5dc2f1c1e8cc
Tenzin Dawa <tnzngdw@gmail.com>
2023-10-15 13:37:20 -0400

    Update JsonUtils.java

--- cowin-report-service/src/main/java/com/example/cowinreportservice/testingunit/utils/JsonUtils.java
+++ cowin-report-service/src/main/java/com/example/cowinreportservice/testingunit/utils/JsonUtils.java
@@ -4,7 +4,6 @@ import com.example.cowinreportservice.testingunit.exceptions.ReportJsonException
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,16 +11,35 @@ import java.io.IOException;
 
 public class JsonUtils {
     private JsonUtils() {
-
+        // Private constructor to prevent instantiation
+        throw new IllegalStateException("Utility class");
     }
 
+    /**
+     * Converts an object to JSON string.
+     *
+     * @param objectToConvert The object to be converted to JSON.
+     * @return JSON string representing the object.
+     * @throws IOException if an error occurs during serialization.
+     */
     public static String toString(Object objectToConvert) throws IOException {
-        return new ObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL).writeValueAsString(objectToConvert);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        return objectMapper.writeValueAsString(objectToConvert);
     }
 
+    /**
+     * Converts a JSON string to an object of the specified class.
+     *
+     * @param jsonString The JSON string to be converted.
+     * @param type The class type to convert the JSON string to.
+     * @param <T> The type of the target object.
+     * @return An object of the specified class.
+     * @throws IOException if an error occurs during deserialization.
+     */
     public static <T> T convertWithClass(String jsonString, Class<T> type) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        return new ObjectMapper().readValue(jsonString, type);
+        return objectMapper.readValue(jsonString, type);
     }
 }
